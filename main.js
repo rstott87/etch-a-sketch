913e3bf72af9f4f77549bfd8f612e02d9bc97993
rstott87 <rstott87@yahoo.com>
2021-10-05 19:24:34 -0400

    made divs be 6.25 of the parent container, so exactly 16 fit across

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
 
-for (i=0; i<16; i++)
+
+
+for (i=0; i<256; i++)
 
 {
 const container = document.querySelector('#container')
 
-const content = document.createElement('div');
+var content = document.createElement('div');
+content.style.border = '1px solid black'; 
+content.style.width = '6.25%';
+content.style.height = 'auto'; 
+content.style.boxSizing = "border-box"
 content.classList.add('content');
-content.textContent = "I'm in a div";
  
 container.appendChild(content);
+
+
 };
+
+let divs = document.querySelectorAll('.content');
+
+divs.forEach((content)  => { 
+
+content.addEventListener ('mouseover', () => {
+    content.style.backgroundColor = "red";
+    });
+
+content.addEventListener ('mouseout', () => {
+    content.style.backgroundColor = "white";
+    });    
+    
+});
+
+/* content.style.backgroundColor = "yellow"; */ 
+
